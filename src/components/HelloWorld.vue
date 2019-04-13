68c4b01682f1f9829509bdf77a46774213b5ef5b
Aldo Troccoli <al2troccoli@gmail.com>
2019-04-13 15:25:05 +0200

    Adding style-resource-loader and establishing styles directory

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="hello testt">
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -40,19 +40,28 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
 }
 ul {
   list-style-type: none;
   padding: 0;
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+    background-color: rosybrown;
+    color: $c1;
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
+    @include atLarge {
+        font-size: 40px;
+        color: $c2;
+    }
 }
+
 </style>
