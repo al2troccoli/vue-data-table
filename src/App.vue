fea7a472a5be89a9b49c7686ff96472418c20278
Aldo Troccoli <al2troccoli@gmail.com>
2019-04-18 16:51:50 +0200

    Css animations and fxs

--- src/App.vue
+++ src/App.vue
@@ -1,110 +1,123 @@
 <template>
-  <div id="app">
-    <!-- CUSTOM GRID -->
-    <transition name="slide-fade">
-        <Grid v-if="appStatus === 'success'"
-          v-bind:has-footer="false"
-          v-bind:has-right-col="false"
-          v-bind:right-col-has-slide="true">
+    <div id="app">
+        <!-- CUSTOM GRID -->
+        <transition name="slide-fade">
+            <Grid v-bind:has-footer="false"
+                  v-bind:has-right-col="false"
+                  v-bind:right-col-has-slide="true">
 
-      <!-- HEADER -->
-      <template v-slot:header>
-        <Header></Header>
-        {{appStatus}}
-        {{user.avatar}}
-      </template>
+                <!-- HEADER -->
+                <template v-slot:header>
+                    <Header></Header>
+                </template>
 
-      <!-- MAIN -->
-      <template v-slot:main>
-        <DataTable :table-name="title"
-                   :columns-array="['term', 'serial_number', 'organization', 'price', 'status']"
-                   :global-selected-asset="currentSelectedAsset"
-                   :assets-array="collections.assets">
+                <!-- MAIN -->
+                <template v-slot:main>
+                    <transition name="slide-fade" mode="out-in">
+                        <!--ERROR MESSAGE-->
+                        <ErrorMessage v-if="appStatus === 'error'"></ErrorMessage>
 
-        </DataTable>
-        <SidePanel v-bind:is-at-right-side="true" >
-          <AssetsForm></AssetsForm>
-        </SidePanel>
-      </template>
+                        <!--DATATABLE-->
+                        <span v-else-if="appStatus === 'success'" class="al2-main__group">
+                            <DataTable :table-name="title"
+                                       :columns-array="['term', 'serial_number', 'organization', 'price', 'status']"
+                                       :global-selected-asset="currentSelectedAsset"
+                                       :assets-array="collections.assets">
+                            </DataTable>
 
-    </Grid>
-    </transition>
+                            <SidePanel v-bind:is-at-right-side="true"
+                                       v-if="appStatus === 'success'">
+                                <AssetsForm></AssetsForm>
+                            </SidePanel>
+                        </span>
 
-    <!--LOADING -->
-    <transition name="slide-fade">
-        <Preloader v-if="appStatus === 'pending'"> </Preloader>
-    </transition>
-
-    <!--ERROR MESSAGE -->
-    <transition name="slide-fade">
-        <ErrorMessage v-if="appStatus === 'error'"></ErrorMessage>
-    </transition>
-  </div>
+                        <!--PRELOADER-->
+                        <Preloader v-if="appStatus === 'pending'"></Preloader>
+                    </transition>
+                </template>
+            </Grid>
+        </transition>
+    </div>
 </template>
 
 <script>
-import DataTable from './components/DataTable.vue'
-import SidePanel from "@/components/SidePanel";
-import Header from "@/components/Header";
-import AssetsForm from "@/components/AssetsForm";
-import Grid from "@/components/Grid";
-import Preloader from "@/components/Preloader";
-import ErrorMessage from "@/components/ErrorMessage";
-import {mapState,mapGetters} from 'vuex';
+    import DataTable from './components/DataTable.vue'
+    import SidePanel from "@/components/SidePanel";
+    import Header from "@/components/Header";
+    import AssetsForm from "@/components/AssetsForm";
+    import Grid from "@/components/Grid";
+    import Preloader from "@/components/Preloader";
+    import ErrorMessage from "@/components/ErrorMessage";
+    import {mapState, mapGetters} from 'vuex';
 
-export default {
-  name: 'app',
-  data() {
-    return {
-      show: true
+    export default {
+        name: 'app',
+        data() {
+            return {
+                show: true
+            }
+        },
+        computed: {
+            ...mapState(['appStatus', 'assets', 'title', 'currentSelectedAsset', 'collections', 'user'])
+        },
+        methods: {
+            ...mapGetters(['getAvatarImgRetina', ['getAvatarImg']])
+        },
+        components: {
+            ErrorMessage,
+            AssetsForm,
+            SidePanel,
+            DataTable,
+            Header,
+            Grid,
+            Preloader
+        },
+        mounted() {
+            this.$store.dispatch('fetchCollections');
+        }
     }
-  },
-  computed: {
-    ...mapState(['appStatus','assets', 'title', 'currentSelectedAsset', 'collections', 'user'])
-  },
-  methods: {
-    ...mapGetters(['getAvatarImgRetina', ['getAvatarImg']])
-  },
-  components: {
-      ErrorMessage,
-      AssetsForm,
-      SidePanel,
-      DataTable,
-      Header,
-      Grid,
-      Preloader
-  },
-  mounted() {
-    this.$store.dispatch('fetchCollections')
-                .then(console.log(this.collections));
-  }
-}
 </script>
 
 <style scoped lang="scss">
-  #app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
+    #app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
 
-  .flex-container {
-    display: flex;
-    flex-direction: row;
-    z-index: z('page-wrapper');
-    overflow-x: hidden;
-    flex-grow: 1;
-  }
+    .al2-main__group {
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        overflow-x: hidden;
+        position: relative;
+        -webkit-overflow-scrolling: touch;
 
-  .slide-fade-enter-active {
-    transition: all .3s ease;
-  }
-  .slide-fade-leave-active {
-    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-  }
-  .slide-fade-enter, .slide-fade-leave-to
-    /* .slide-fade-leave-active below version 2.1.8 */ {
-    transform: translateX(10px);
-    opacity: 0;
-  }
+        @include atSmall {
+            flex-direction: row;
+        }
+    }
+
+    .flex-container {
+        display: flex;
+        flex-direction: row;
+        z-index: z('page-wrapper');
+        overflow-x: hidden;
+        flex-grow: 1;
+    }
+
+    .slide-fade-enter-active {
+        transition: all .3s ease;
+    }
+
+    .slide-fade-leave-active {
+        transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+
+    .slide-fade-enter, .slide-fade-leave-to
+        /* .slide-fade-leave-active below version 2.1.8 */
+    {
+        transform: translateX(10px);
+        opacity: 0;
+    }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,149 +1,149 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
-import VueAxios  from 'vue-axios';
+import VueAxios from 'vue-axios';
 
 Vue.use(Vuex);
 Vue.use(VueAxios, axios);
 
 export default new Vuex.Store({
-  state: {
-    appStatus: 'pending',
-    collections: [],
-    assets: [
-      {
-        term: "accessory tray",
-        serial_number: "SN00009582200A4",
-        organization: "Celestron",
-        price: "1234",
-        status: 0,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      },
-      {
-        term: "achromat",
-        serial_number: "SN0000458925329",
-        organization: "Skywatcher",
-        price: "4546",
-        status: 1,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      },
-      {
-        term: "apochromatic",
-        serial_number: "SN0000458925330",
-        organization: "Coronado",
-        price: "2534",
-        status: 0,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      },
-      {
-        term: "baffles",
-        serial_number: "TN0000458925329",
-        organization: "Meade",
-        price: "4345",
-        status: 1,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      },
-      {
-        term: "BaK-4 glass",
-        serial_number: "SN0000458925332",
-        organization: "Vixen",
-        price: "3345",
-        status: 1,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      },
-      {
-        term: "barlow lens",
-        serial_number: "SN0000458925333",
-        organization: "Orion",
-        price: "2345",
-        status: 0,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      },
-      {
-        term: "binocular",
-        serial_number: "TK0000458925329",
-        organization: "Omegon",
-        price: "1234",
-        status: 1,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      },
-      {
-        term: "camera adapter",
-        serial_number: "SN0000506075329",
-        organization: "IntesMicro",
-        price: "2324",
-        status: 0,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      },
-      {
-        term: "ccd camera",
-        serial_number: "SN0000506075338",
-        organization: "APM",
-        price: "2345",
-        status: 0,
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      }
-    ],
-    assetsFiltered: [],
-    user: {},
-    title: "Hubble`s assets",
-    currency: "EUR",
-    currentSelectedAsset: null
-  },
-  mutations: {
-    updateSelectedAsset(state, index) {
-      state.currentSelectedAsset = index;
+    state: {
+        appStatus: 'pending',
+        collections: [],
+        assets: [
+            {
+                term: "accessory tray",
+                serial_number: "SN00009582200A4",
+                organization: "Celestron",
+                price: "1234",
+                status: 0,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            },
+            {
+                term: "achromat",
+                serial_number: "SN0000458925329",
+                organization: "Skywatcher",
+                price: "4546",
+                status: 1,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            },
+            {
+                term: "apochromatic",
+                serial_number: "SN0000458925330",
+                organization: "Coronado",
+                price: "2534",
+                status: 0,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            },
+            {
+                term: "baffles",
+                serial_number: "TN0000458925329",
+                organization: "Meade",
+                price: "4345",
+                status: 1,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            },
+            {
+                term: "BaK-4 glass",
+                serial_number: "SN0000458925332",
+                organization: "Vixen",
+                price: "3345",
+                status: 1,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            },
+            {
+                term: "barlow lens",
+                serial_number: "SN0000458925333",
+                organization: "Orion",
+                price: "2345",
+                status: 0,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            },
+            {
+                term: "binocular",
+                serial_number: "TK0000458925329",
+                organization: "Omegon",
+                price: "1234",
+                status: 1,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            },
+            {
+                term: "camera adapter",
+                serial_number: "SN0000506075329",
+                organization: "IntesMicro",
+                price: "2324",
+                status: 0,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            },
+            {
+                term: "ccd camera",
+                serial_number: "SN0000506075338",
+                organization: "APM",
+                price: "2345",
+                status: 0,
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            }
+        ],
+        assetsFiltered: [],
+        user: {},
+        title: "Hubble`s assets",
+        currency: "EUR",
+        currentSelectedAsset: null
     },
-    resetSelectedItem(state) {
-      state.currentSelectedAsset = null;
+    mutations: {
+        updateSelectedAsset(state, index) {
+            state.currentSelectedAsset = index;
+        },
+        resetSelectedItem(state) {
+            state.currentSelectedAsset = null;
+        },
+        updateFilteredAssetsArray(state, filteredArray) {
+            state.assetsFiltered = filteredArray;
+        },
+        setColletions(state, payload) {
+            state.collections = payload;
+            state.title = payload.title.title;
+            state.columnsNames = payload.columns;
+            state.currency = payload.currency.currency;
+            state.user = payload.user;
+            console.log(state.collections)
+        },
+        updateAppStatus(state, payload) {
+            state.appStatus = payload;
+        }
     },
-    updateFilteredAssetsArray(state, filteredArray) {
-      state.assetsFiltered = filteredArray;
+    getters: {
+        getCurrency(state) {
+            return state.currency === "EUR" ? "€" : "$";
+        },
+        getAvatarImg(state) {
+            return state.user.avatar;
+        },
+        getAvatarImgRetina(state) {
+            let extensionPosition = state.user.avatar.indexOf(".");
+            console.log(extensionPosition + "   ssssssssssssssssss")
+            return extensionPosition;
+        }
     },
-    setColletions(state, payload) {
-      state.collections = payload;
-      state.title= payload.title.title;
-      state.columnsNames= payload.columns;
-      state.currency = payload.currency.currency;
-      state.user = payload.user;
-      console.log(state.collections)
-    },
-    updateAppStatus(state, payload) {
-      state.appStatus = payload;
-    }
-  },
-  getters: {
-    getCurrency(state) {
-      return state.currency === "EUR" ? "€" : "$";
-    },
-    getAvatarImg(state) {
-      return state.user.avatar;
-    },
-    getAvatarImgRetina(state) {
-      let extensionPosition = state.user.avatar.indexOf(".");
-      console.log(extensionPosition + "   ssssssssssssssssss")
-      return extensionPosition;
-    }
-  },
-  actions: {
-    async fetchCollections( {commit}) {
-      try {
-        await Vue.axios
-            .get("http://localhost:3000/dbXXXXXX")
-            .then(response => commit('setColletions', response.data));
+    actions: {
+        async fetchCollections({commit}) {
+            try {
+                await Vue.axios
+                    .get("http://localhost:3000/db")
+                    .then(response => commit('setColletions', response.data));
 
-        // NOTE: This timeout is for showing the preloader component
-        setTimeout(function(){
-          commit('updateAppStatus', 'success');
-        }, 2000)
+                // NOTE: This timeout is for showing the preloader component
+                setTimeout(function () {
+                    commit('updateAppStatus', 'success');
+                }, 2000)
 
-      } catch (e) {
-        console.error(e)
-        commit('updateAppStatus', 'error');
-      }
-      
+            } catch (e) {
+                console.error(e);
+                commit('updateAppStatus', 'error');
+            }
 
+
+        }
     }
-  }
 })
 
