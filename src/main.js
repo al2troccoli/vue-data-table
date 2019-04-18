3824a15be300a7e08e2b54441e6bd1f0855f874f
Aldo Troccoli <al2troccoli@gmail.com>
2019-04-18 08:50:30 +0200

    Refactoring

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import VueFire from 'vuefire'
-import './firebase'
 // main styles
 require('./styles/main.scss');
 
 Vue.config.productionTip = false;
-Vue.use(VueFire);
-
 
 new Vue({
   store,
